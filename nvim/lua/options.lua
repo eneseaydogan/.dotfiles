a638691eb2c09c5eed06ea3e2cc1ffcfa5881b43
Enes Emre Aydogan <enes.e.aydogan@gmail.com>
2025-09-03 16:57:50 +0300

    feat(nvim): add options

---
+++ nvim/lua/options.lua
@@ -0,0 +1,54 @@
+vim.o.laststatus = 3
+vim.o.colorcolumn = "100"
+vim.o.showmode = false
+vim.o.cursorline = true
+vim.o.number = true
+vim.o.relativenumber = true
+vim.o.signcolumn = "yes"
+vim.o.scrolloff = 8
+vim.o.sidescrolloff = 8
+vim.o.wrap = true
+vim.o.termguicolors = true
+
+vim.o.expandtab = true
+vim.o.shiftwidth = 2
+vim.o.tabstop = 2
+vim.o.softtabstop = 2
+vim.o.smartindent = true
+
+vim.o.ignorecase = true
+vim.o.smartcase = true
+
+vim.o.undofile = true
+vim.o.updatetime = 200
+vim.o.autowrite = true
+
+vim.o.mouse = "a"
+vim.o.clipboard = "unnamedplus"
+vim.o.timeoutlen = 300
+
+vim.g.loaded_node_provider = 0
+vim.g.loaded_python3_provider = 0
+vim.g.loaded_perl_provider = 0
+vim.g.loaded_ruby_provider = 0
+
+vim.diagnostic.config({
+  severity_sort = true,
+  float = {
+    focusable = true,
+    style = "minimal",
+    border = "none",
+    header = "",
+    prefix = "",
+  },
+  virtual_text = true,
+  virtual_lines = { current_line = true },
+  signs = {
+    text = {
+      [vim.diagnostic.severity.ERROR] = " ",
+      [vim.diagnostic.severity.WARN] = " ",
+      [vim.diagnostic.severity.INFO] = " ",
+      [vim.diagnostic.severity.HINT] = " ",
+    },
+  },
+})
