741e150e9aa774d270ef2bdcf1fea2ee4c61b3d6
Enes Emre Aydogan <enes.e.aydogan@gmail.com>
2025-09-03 19:38:33 +0300

    feat(nvim): add lazy.nvim

--- nvim/init.lua
+++ nvim/init.lua
@@ -2,6 +2,7 @@ vim.loader.enable()
 vim.keymap.set("n", "<Space>", "<Nop>", { silent = true })
 vim.g.mapleader = " "
 vim.g.maplocalleader = ","
+require("lazy")
 require("options")
 require("maps")
 require("autocmds")
