a6f01b93e9ba2a944fd57acfa1db158b86b70339
Enes Emre Aydogan <enes.e.aydogan@gmail.com>
2025-09-12 00:46:09 +0300

    bootstrap

---
+++ bootstrap.fish
@@ -0,0 +1,42 @@
+#!/usr/bin/env fish
+set -e
+
+echo "[*] Updating system..."
+sudo pacman -Syu --noconfirm
+
+echo "[*] Installing paru..."
+if not type -q paru
+    git clone https://aur.archlinux.org/paru.git /tmp/paru
+    cd /tmp/paru
+    makepkg -si --noconfirm
+    cd -
+end
+
+echo "[*] Installing packages from packages.txt..."
+paru -S --needed --noconfirm (cat packages.txt)
+
+echo "[*] Cloning repos into bootstrap/..."
+set bootstrap_dir "$HOME/.dotfiles/bootstrap"
+mkdir -p $bootstrap_dir
+
+for repo in (cat repos.txt)
+    if test -z "$repo"
+        continue
+    end
+    set name (basename $repo .git)
+    set dir "$bootstrap_dir/$name"
+
+    if test -d $dir
+        echo "  [=] Updating $name"
+        git -C $dir pull
+    else
+        echo "  [+] Cloning $repo"
+        git clone $repo $dir
+    end
+end
+
+echo "[*] Linking configs with stow..."
+cd (dirname (status -f))  # go to .dotfiles repo root
+stow -t $HOME fish nvim alacritty
+
+echo "[*] Done!"
